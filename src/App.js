36cbbf84abafafd83e8f2319b116bf6d6aee2e76
AR <arief0303@gmail.com>
2021-04-29 18:05:17 +0200

    Canvas setup and drawing sharp text in 3D Space

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import "./App.scss";
 //Components
 import Header from "./components/header";
+import { Text } from '@react-three/drei'
+import { Canvas } from "@react-three/fiber";
+
+function Title() {
+   return <Text material-toneMapped={false} {...TEXT_PROPS}>TALON</Text>
+}
+
+const TEXT_PROPS = {
+  fontSize: 1,
+  font: 'https://fonts.gstatic.com/s/syncopate/v12/pe0pMIuPIYBCpEV5eFdKvtKqBP5p.woff'
+}
 
 export default function App() {
   return (
     <>
       <Header />
-      <div className='container'>
-        <div className='title'>
-          <h1>Talon</h1>
-        </div>
-      </div>
+      <Canvas>
+        <Title />
+      </Canvas>
     </>
   );
 }
