4bed4cb023fecb8776c5ab509490cc99315c84fc
arief0303 <arief0303@gmail.com>
2021-04-29 18:21:28 +0200

    camera z index set and set reference object

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import React from "react";
 import "./App.scss";
 //Components
 import Header from "./components/header";
-import { Text } from '@react-three/drei'
+import { Text } from '@react-three/drei';
+import { RoundedBox } from '@react-three/drei';
 import { Canvas } from "@react-three/fiber";
-
-function Title() {
-   return <Text material-toneMapped={false} {...TEXT_PROPS}>TALON</Text>
-}
 
 const TEXT_PROPS = {
   fontSize: 1,
   font: 'https://fonts.gstatic.com/s/syncopate/v12/pe0pMIuPIYBCpEV5eFdKvtKqBP5p.woff'
 }
 
+function Title() {
+   return <Text material-toneMapped={false} {...TEXT_PROPS}>TALON</Text>
+}
+
 export default function App() {
   return (
     <>
       <Header />
-      <Canvas>
+      <Canvas camera={{ position: [0, 0, 5], fov: 70 }}>
         <Title />
+        <RoundedBox args={[1, 1, 1]} radius={0.05} smoothness={4}>
+          <meshPhongMaterial attach="material" color="#f3f3f3" wireframe />
+        </RoundedBox>
       </Canvas>
     </>
   );
