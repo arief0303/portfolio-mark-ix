d110a7bb0fd2956c8ed7746d5f5ea3937fb79285
Arief <arief0303@gmail.com>
2021-05-01 00:58:39 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import "./App.scss";
 import React from "react";
 import Header from "./components/header";
 import { Canvas } from "@react-three/fiber";
-import { Text } from '@react-three/drei';
-import { RoundedBox } from '@react-three/drei';
-import { Section } from "./components/section"
+import { Text, RoundedBox } from "@react-three/drei";
+import { Section } from "./components/section";
 
 const TEXT_PROPS = {
   fontSize: 1,
-  font: 'https://fonts.gstatic.com/s/syncopate/v12/pe0pMIuPIYBCpEV5eFdKvtKqBP5p.woff'
-}
+  font:
+    "https://fonts.gstatic.com/s/syncopate/v12/pe0pMIuPIYBCpEV5eFdKvtKqBP5p.woff",
+};
 
 function Title() {
-   return <Text material-toneMapped={false} {...TEXT_PROPS}>TALON</Text>
+  return (
+    <Text material-toneMapped={false} {...TEXT_PROPS}>
+      TALON
+    </Text>
+  );
 }
 
 export default function App() {
@@ -21,16 +25,15 @@ export default function App() {
     <>
       <Header />
       <Canvas camera={{ position: [0, 0, 5], fov: 70 }}>
-          <Section factor={1} offset={0}>
-            <group position={[0,0,0]}>
+        <Section factor={1} offset={0}>
+          <group position={[0, 0, 0]}>
             <Title />
-              <RoundedBox args={[1, 1, 1]} radius={0.05} smoothness={4}>
+            <RoundedBox args={[1, 1, 1]} radius={0.05} smoothness={4}>
               <meshPhongMaterial attach="material" color="#f3f3f3" wireframe />
-              </RoundedBox>
-            </group>
-          </Section>
-          {/* <Block factor={1.2} offset={5.7}> */}
-  
+            </RoundedBox>
+          </group>
+        </Section>
+        {/* <Block factor={1.2} offset={5.7}> */}
       </Canvas>
     </>
   );
