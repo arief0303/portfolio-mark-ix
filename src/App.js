804ca6cb858aa29ce0b8b8d8fb87ac2310dcb8f4
arief0303 <arief0303@gmail.com>
2021-05-03 18:16:15 +0200

    Title adjust

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //Components
 import "./App.scss";
 import * as THREE from "three";
-import React, { useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Header from "./components/header";
 import { Canvas, useFrame, useResource } from "react-three-fiber";
 import { Text, Box } from "@react-three/drei";
+import { OrbitControls, Sky } from "@react-three/drei";
 // import { useMatcapTexture, Octahedron } from "@react-three/drei";
 import { Section } from "./components/section";
 
@@ -16,17 +17,34 @@ import useLayers from "./use-layers";
 // import useRenderTarget from "./use-render-target";
 
 const TEXT_PROPS = {
-  fontSize: 1,
+  fontSize: 2.5,
   font:
     "https://fonts.gstatic.com/s/syncopate/v12/pe0pMIuPIYBCpEV5eFdKvtKqBP5p.woff",
 };
 
-function Title() {
+// function Title() {
+//   return (
+//     <Text material-toneMapped={false} {...TEXT_PROPS}>
+//       TALON
+//     </Text>
+//   );
+// }
+
+function Title({ layers, ...props }) {
+  const group = useRef()
+  useEffect(() => {
+    group.current.lookAt(0, 0, 0)
+  }, [])
+
+  const textRef = useLayers(layers)
+
   return (
-    <Text material-toneMapped={false} {...TEXT_PROPS}>
-      TALON
-    </Text>
-  );
+    <group {...props} ref={group}>
+      <Text ref={textRef} name="text-talon" depthTest={false} material-toneMapped={false} material-color="#FFFFFF" {...TEXT_PROPS}>
+        TALON
+      </Text>
+    </group>
+  )
 }
 
 function Mirror({ sideMaterial, reflectionMaterial, args, layers, ...props }) {
@@ -113,38 +131,54 @@ function Mirrors({ envMap, layers, ...props }) {
 //   );
 // };
 
-export default function App() {
+function Scene() {
   const [renderTarget] = useState(new THREE.WebGLCubeRenderTarget(1024));
   const cubeCamera = useRef();
 
-  // SOLVE ERROR
-  // useFrame(({ gl, scene }) => {
-  //   cubeCamera.current.update(gl, scene);
-  // });
+  useFrame(({ gl, scene }) => {
+    cubeCamera.current.update(gl, scene);
+  });
 
+  return (
+    <>
+      <Section factor={1} offset={0}>
+        <group position={[0, 0, 0]}>
+          <Sky
+            layers={[11]}
+            sunPosition={[Math.PI, 0, Math.PI / 2]}
+            turbidity={8}
+            rayleigh={6}
+            mieCoefficient={0.0005}
+            mieDirectionalG={0.8}
+          />
+          <cubeCamera
+            layers={[11]}
+            name="cubeCamera"
+            ref={cubeCamera}
+            position={[0, 0, 0]}
+            // i. notice how the renderTarget is passed as a constructor argument of the cubeCamera object
+            args={[0.1, 100, renderTarget]}
+          />
+          <Title name="title" position={[0, 0, -10]} />
+          {/* <Mirrors /> */}
+          <Mirrors layers={[0, 11]} envMap={renderTarget.texture} />
+          {/* <Lights /> */}
+          <OrbitControls />
+          <ambientLight intensity={0.4} />
+        </group>
+      </Section>
+      {/* <Block factor={1.2} offset={5.7}> */}
+    </>
+  );
+}
+
+export default function App() {
   return (
     <>
       <Header />
       {/* <Canvas camera={{ position: [0, 0, 5], fov: 70 }}> */}
       <Canvas concurrent shadowMap camera={{ position: [0, 0, 5], fov: 70 }}>
-        <Section factor={1} offset={0}>
-          <group position={[0, 0, 0]}>
-            <cubeCamera
-              layers={[11]}
-              name="cubeCamera"
-              ref={cubeCamera}
-              position={[0, 0, 0]}
-              // i. notice how the renderTarget is passed as a constructor argument of the cubeCamera object
-              args={[0.1, 100, renderTarget]}
-            />
-            <Title />
-            {/* <Mirrors /> */}
-            <Mirrors layers={[0, 11]} envMap={renderTarget.texture} />
-            {/* <Lights /> */}
-            <ambientLight intensity={0.4} />
-          </group>
-        </Section>
-        {/* <Block factor={1.2} offset={5.7}> */}
+        <Scene />
       </Canvas>
     </>
   );
