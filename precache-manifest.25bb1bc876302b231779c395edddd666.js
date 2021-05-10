da9f16cf1a06d139b857cee1985ba3e0ed590ee5
arief0303 <arief0303@gmail.com>
2021-05-10 22:13:01 +0200

    Updates

---
+++ precache-manifest.25bb1bc876302b231779c395edddd666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5dd8a7b7ec751d6ae517c0d7958a45",
+    "url": "/portfolio-mark-x/index.html"
+  },
+  {
+    "revision": "5bcaacbe2c99be7f30cf",
+    "url": "/portfolio-mark-x/static/css/main.7288ecee.chunk.css"
+  },
+  {
+    "revision": "513584b9fb3975389722",
+    "url": "/portfolio-mark-x/static/js/2.049fb18c.chunk.js"
+  },
+  {
+    "revision": "4a39ab3be31c11d2dea3d04487e78b26",
+    "url": "/portfolio-mark-x/static/js/2.049fb18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcaacbe2c99be7f30cf",
+    "url": "/portfolio-mark-x/static/js/main.b319cb4f.chunk.js"
+  },
+  {
+    "revision": "d5c1d3fd7d9ce862065e",
+    "url": "/portfolio-mark-x/static/js/runtime-main.50573b4b.js"
+  }
+]);
